fea3c447250b2b55963c343eed7c4d2d32e2c741
Nayee22 <140297710+Nayee22@users.noreply.github.com>
2024-01-22 00:13:28 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -5,11 +5,11 @@ throw false
 }
 let vn = './media/Invocar.mp3'
 let pesan = args.join` `
-let oi = `*𝙈𝙚𝙣𝙨𝙖𝙟𝙚:* ${pesan}`
-let teks = `╭┄〔 *${wm}* 〕┄⊱\n┆ 𝘼𝙘𝙩𝙞𝙫𝙚𝙣𝙨𝙚 𝙥𝙡𝙖𝙣𝙩𝙖𝙨 🙊*\n┆\n┆❏ ${oi}\n┆\n┆❏ *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
+let oi = `*𝘕𝘪𝘯𝘨ú𝘯 𝘮𝘢𝘳 𝘦𝘯 𝘤𝘢𝘭𝘮𝘢 𝘩𝘪𝘻𝘰 𝘦𝘹𝘱𝘦𝘳𝘵𝘰 𝘢 𝘶𝘯 𝘮𝘢𝘳𝘪𝘯𝘦𝘳𝘰.* ${pesan}`
+let teks = `╭┄〔 *${wm}* 〕┄⊱\n┆ 💠 • 𝙄𝙉𝙑𝙊𝘾𝘼𝙉𝘿𝙊 𝙂𝙍𝙐𝙋𝙊 • 💠*\n┆\n┆❏ ${oi}\n┆\n┆❏ *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
 for (let mem of participants) {
-teks += `┆🙈 @${mem.id.split('@')[0]}\n`}
-teks += `└ @𝐦𝐨𝐧𝐤𝐢_𝐛𝐨𝐭 🐵 \n\n`
+teks += `🫧▪︎.  @${mem.id.split('@')[0]}\n`}
+teks += `└ 𝙊𝙘𝙚𝙖𝙣 𝘾𝙤𝙧𝙥 🩵🌀•. \n\n`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
